c7d39a61ad31d05dcdae3b3a8cc56d54142e2fd2
zideajang <zidea2015@163.com>
2023-06-03 06:46:53 +0800

    实现绘制矩形方法

--- example.c
+++ example.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<stdint.h>
 #include<error.h>
+#include<string.h>
 
 #include "zencil.c"
 
@@ -11,61 +12,42 @@
 // 定义
 static uint32_t pixels[WIDTH*HEIGHT];
 
-typedef int Errno;
-// 1,2,3,4,5,6 ,width 2 height 3 
 
-void zencil_fill(uint32_t *pixels, size_t width, size_t height, uint32_t color)
+// 绘制矩形
+void zencil_fill_rect(uint32_t *pixels,size_t pixels_width,size_t pixels_height, 
+    int x0,int y0, size_t w, size_t h,uint32_t color )
 {
-    for (size_t i = 0; i < width*height; i++)
+    for (int dy = 0; dy < (int)h; dy++)
     {
-        pixels[i] = color;
-    }
-
-    
-}
-
-#define return_defer(value) do {result=(value);goto defer;} while(0)
-
-Errno zencil_save_to_ppm_file(uint32_t *pixels, size_t width ,size_t height, const char*file_path)
-{
-    int result = 0;
-    FILE *f = NULL;
-    {
-        // 打开文件
-        f = fopen(file_path,"wb");
-        
-        if(f == NULL) return_defer(errno);
-        
-        // 读取文件
-        fprintf(f,"P6\n%zu %zu 255\n",width,height);
-
-        if(ferror(f)) return_defer(errno);
-
-        for(size_t i =0; i < width*height; i++){
-            // 0xAARRGGBB
-            uint32_t pixel = pixels[i];
-            uint8_t bytes[3] = {
-                (pixel>>(8*0))&0xFF,
-                (pixel>>(8*1))&0xFF,
-                (pixel>>(8*2))&0xFF,
-            };
-
-            fwrite(bytes,sizeof(bytes),1,f);
-            if(ferror(f)) return_defer(errno);
-
+        int y = y0 + dy;//row
+        if(0 <= y && y < (int)pixels_height){
+            for (int dx = 0; dx < (int)w; dx++)
+            {
+                int x = x0 + dx;
+                if(0 <= x && x < (int)pixels_width){
+                    pixels[y*pixels_width + x] = color;
+                }
+            }
         }
-
+        
     }
-
-defer:
-    if(f) fclose(f);
-    return result;
+    
 }
 
 int main(int argc, char const *argv[])
 {
-    zencil_fill(pixels,WIDTH,HEIGHT,0xFF00FF00);
-    zencil_save_to_ppm_file(pixels,WIDTH,HEIGHT,"output.ppm");
+    zencil_fill(pixels,WIDTH,HEIGHT,0xFF202020);
+
+    zencil_fill_rect(pixels,WIDTH,HEIGHT,0,0,100,50,0xFF2020FF);
+
+    const char *file_path = "output.ppm";
+    
+    Errno err = zencil_save_to_ppm_file(pixels,WIDTH,HEIGHT,file_path);
+    
+    if(err){
+        fprintf(stderr,"ERROR: could not save file:%s: %s\n",file_path);
+        return 1;
+    }
     return 0;
     
 }
--- zencil.c
+++ zencil.c
@@ -1,5 +1,55 @@
 #ifndef ZENCIL_H_
 #define ZENCIL_H_
+typedef int Errno;
+// 1,2,3,4,5,6 ,width 2 height 3 
+
+void zencil_fill(uint32_t *pixels, size_t width, size_t height, uint32_t color)
+{
+    for (size_t i = 0; i < width*height; i++)
+    {
+        pixels[i] = color;
+    }
+
+    
+}
+
+#define return_defer(value) do {result=(value);goto defer;} while(0)
+
+Errno zencil_save_to_ppm_file(uint32_t *pixels, size_t width ,size_t height, const char*file_path)
+{
+    int result = 0;
+    FILE *f = NULL;
+    {
+        // 打开文件
+        f = fopen(file_path,"wb");
+        
+        if(f == NULL) return_defer(errno);
+        
+        // 读取文件
+        fprintf(f,"P6\n%zu %zu 255\n",width,height);
+
+        if(ferror(f)) return_defer(errno);
+
+        for(size_t i =0; i < width*height; i++){
+            // 0xAARRGGBB
+            uint32_t pixel = pixels[i];
+            uint8_t bytes[3] = {
+                (pixel>>(8*0))&0xFF,
+                (pixel>>(8*1))&0xFF,
+                (pixel>>(8*2))&0xFF,
+            };
+
+            fwrite(bytes,sizeof(bytes),1,f);
+            if(ferror(f)) return_defer(errno);
+
+        }
+
+    }
+
+defer:
+    if(f) fclose(f);
+    return result;
+}
 
 
 #endif //ZENCIL_H_
