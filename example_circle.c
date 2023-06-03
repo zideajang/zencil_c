e011b9af7688198055b93ec8a3ea1926c784cf7c
zideajang <zidea2015@163.com>
2023-06-03 15:13:56 +0800

    添加绘制线

--- example_circle.c
+++ example_circle.c
@@ -25,73 +25,10 @@
 static uint32_t pixels[WIDTH*HEIGHT];
 
 
-void swap_int(int *a, int *b)
-{
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
+// 绘制 circle ，位置(x,y) 半径(radius)
+// pixels [] height(y) width(x) 
+// without depencies
 
-void zencil_draw_line(uint32_t *pixels, size_t pixels_width, size_t pixels_height,
-    int x1,int y1,int x2,int y2,
-    uint32_t color)
-{
-    int dx = x2 - x1;
-    int dy = y2 - y1;
-
-    if(dx != 0) {
-        // float k = (float)dy/(float)dx;
-        float c = y1 - dy*x1/dx;
-
-        if(x1 > x2) swap_int(&x1, &x2); 
-        for(int x = x1; x <= x2; x++){
-
-            if(0 <= x && x < (int)pixels_width){
-                int nx = x + 1;
-            
-                int sy1 = dy*x/dx + c;
-                int sy2 = dy*(x + 1)/dx + c; 
-                if(sy1 > sy2) swap_int(&sy1,&sy2);
-                for(int y = sy1; y <= sy2; y++ ){
-                    if(0 <= y && y < (int)pixels_height){
-                        pixels[y*pixels_width + x] = color;
-                    }
-                }
-            }
-        }
-    }else{
-        int x = x1;
-        if(0 <= x && x < pixels_width){
-            if(y1 < y2) swap_int(&y1,&y2);
-            for(int y = y1; y <= y2; y++){
-                if(0 <= y && y < pixels_height){
-                    pixels[y*pixels_width + x] = color;
-                }
-            }
-        }
-    }
-}
-
-bool lines_example(void)
-{
-    zencil_fill(pixels,WIDTH,HEIGHT,BACKGROUND_COLOR);
-
-    zencil_draw_line(pixels,WIDTH,HEIGHT,
-    0,0,WIDTH/4,HEIGHT,FOREGROUND_COLOR);
-
-    zencil_draw_line(pixels,WIDTH,HEIGHT,
-    WIDTH,0,WIDTH/4*3,HEIGHT,FOREGROUND_COLOR);
-
-    const char *file_path = "lines_example.ppm";
-    
-    Errno err = zencil_save_to_ppm_file(pixels,WIDTH,HEIGHT,file_path);
-    
-    if(err){
-        fprintf(stderr,"ERROR: could not save file:%s: %s\n",file_path);
-        return false;
-    }
-    return true;
-}
 
 bool checkboard_example(void){
 
@@ -174,9 +111,7 @@ bool circle_example(void)
 int main(int argc, char const *argv[])
 {
     // if(!checkboard_example()) return -1;
-    // if(!circle_example()) return -1;
-
-    if(!lines_example()) return -1;
+    if(!circle_example()) return -1;
     return 0;
     
 }
